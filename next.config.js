6b21d8498b1c45f5dfb94f078d7b7b3dfd19146f
Denis Pablo <denis.pablo@cuida.pe>
2022-06-03 00:12:46 -0500

    [Feat] Add confix axios

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
-  }
+  },
+  env: {
+    apiGitHubUrl: 'https://api.github.com/',
+  },
 }
 
 module.exports = nextConfig
